42db903bd6a38279636000b1ad2a15fa683cb2c0
DahnDell <khanman1208@gmail.com>
2024-12-07 18:59:40 +0900

    baekjoon_1000A+B

---
+++ baekjoon1000AB.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class baekjoon1000AB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = 0;
+        while(A <= 0 || 10 <= A){
+            A = scanner.nextInt();
+        }
+        
+        int B = 0;
+        while(B <= 0 || 10 <= B){
+            B = scanner.nextInt();
+        }
+
+        System.out.println(A + B);
+        scanner.close();   
+    }
+}
