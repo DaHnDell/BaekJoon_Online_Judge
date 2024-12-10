ba0d1c6d983c6f9d5f46795ce8b7b5054be5fe05
DahnDell <khanman1208@gmail.com>
2024-12-10 23:36:55 +0900

    rename files

--- _1003fibonacci.java
+++ _1003fibonacci.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class _1003fibonacci {
+public class _1003Fibonacci {
     public static void main(String[] args) {
         // System.out.println();
         Scanner scanner = new Scanner(System.in);
